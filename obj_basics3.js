77620d3ae35de0fd0336baec01f5a971a77dc296
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-11 15:45:48 +0530

    Update obj_basics3.js at line 16 i assigned the empty object instead of declaring so caught an error (Resolved)

--- obj_basics3.js
+++ obj_basics3.js
@@ -13,5 +13,5 @@ console.log(Student.addStud);
 //here we have to write the object property name with dot operator again and again 
 //lets destructur it
 
-const = { addStud : add, firstName : FN, lastName : LN, clgID : ID} = Student;  
+const  { addStud : add, firstName : FN, lastName : LN, clgID : ID} = Student;  
 consol.log(add);
