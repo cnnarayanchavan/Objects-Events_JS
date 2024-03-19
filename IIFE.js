22a07d7db79a4423d44d3935b1950e99b0ae8e09
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-19 16:51:58 +0530

    Create IIFE.js initialized iife

---
+++ IIFE.js
@@ -0,0 +1,3 @@
+// Understanding what is IIFE[Immediately Invoked Function Executation]
+
+//we use this method IIFE to get rid of global scope pollution (where teh decleared variables stucks sometimes)
