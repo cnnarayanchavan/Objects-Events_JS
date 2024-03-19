2e94aca3f472952315eabc8f922977f8853aac6c
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-19 16:55:27 +0530

    Update IIFE.js viewed differences

--- IIFE.js
+++ IIFE.js
@@ -1,3 +1,17 @@
 // Understanding what is IIFE[Immediately Invoked Function Executation]
 
 //we use this method IIFE to get rid of global scope pollution (where teh decleared variables stucks sometimes)
+
+function greetMsg(username){
+  console.log(` ${username}, your db is connected`)
+}
+greetMsg("Narayana");
+
+//this is the common way of defining the function 
+
+(function greetMsg2(username){
+  console.log(` ${username}, your db2 is connected`)
+})("Nitin")
+
+
+
