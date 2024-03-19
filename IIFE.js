24c9ea3d1ef9c1578ece9464a4fcaf91d75e631d
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-19 17:00:49 +0530

    Update IIFE.js_ mod 2

--- IIFE.js
+++ IIFE.js
@@ -13,5 +13,21 @@ greetMsg("Narayana");
   console.log(` ${username}, your db2 is connected`)
 })("Nitin")
 
+//so here this is the IIEF where we called function immediatelt using parenthesis.
+
+//now we waht to print three function in one go 
+
+// (function greetMsg(username){
+//   console.log(` ${username}, your db2 is connected`)
+// })("Nitin")
+
+//lets make this arrow function 
+
+(((username)=>{
+  console.log(` ${username}, your db2 is connected`)
+})("Nitin")
+
+
+
 
 
