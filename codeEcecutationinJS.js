3a01f66adb3e7b4e363629f18f2444d00b7cc2ed
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-21 21:12:21 +0530

    Create codeEcecutationinJS.js_ learmimg how code executes in js

---
+++ codeEcecutationinJS.js
@@ -0,0 +1,16 @@
+//How java script executes the code 
+//if we write any code in js , for beginning it will has these steps 
+
+let num1 =12
+let num2 =23
+function add2Num(val1, val2){
+  let result = val1 + val2;
+  returen result;
+}
+add2Num(num1, num2);
+add2Num(45,45);
+
+
+              //SO HERE
+
+              //first Global Ex_Fun and assign to =========== this(keyword)
