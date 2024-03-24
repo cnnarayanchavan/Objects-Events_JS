f23c393438e084d6abed7e51e098b26fa70b329a
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-24 21:55:34 +0530

    Update HigherOrder.js understand the for each

--- HigherOrder.js
+++ HigherOrder.js
@@ -35,3 +35,11 @@ for (const [i,j] of map)
     console.log(i , ":-" , j );         //this is special syntax
 }   
 
+
+
+//Higher Order Function
+// 1] ForEach (callback function) callback function has no name 
+
+// in this for each function (call back function) 
+// if we are defining the function first outside the for each loop and then add it into the for each higher order function 
+//then we have to pass it by REFERENCE not by VALUE     because we have to pass it not executes it 
