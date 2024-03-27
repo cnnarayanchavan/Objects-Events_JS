25b6e6ae402b0dea4c53b18badf360a5c16f7daf
cnnarayanchavan <cnnarayanchavan@gamil.com>
2024-03-27 23:31:44 +0530

    Added modified file

--- TestPage3.js
+++ TestPage3.js
@@ -1,18 +1,25 @@
-//recalling the arrow function
+// //recalling the arrow function
 
-function greet(){
-   // console.log("Hello world!");
-}
-greet()
+// function greet(){
+//    // console.log("Hello world!");
+// }
+// greet()
 
-Narayan=(greet) => 
-{
-    if (!greet) 
-    {
-        console.log("please enter msg");
-        return
-    }
-    console.log(`Hello ${greet}`);
+// Narayan=(greet) => 
+// {
+//     if (!greet) 
+//     {
+//         console.log("please enter msg");
+//         return
+//     }
+//     console.log(`Hello ${greet}`);
+// }
+// //Narayan();
+// Narayan("Narayana");
+
+a =5;
+count=0
+for(i=1;i<=5;i++){
+    count=count+i
 }
-//Narayan();
-Narayan("Narayana");
+console.log(count);
