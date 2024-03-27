9a66665542f25e62b755ce5fa86f8160ba635201
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-27 23:24:35 +0530

    Create FilterandMap.js_higher order function

---
+++ FilterandMap.js
@@ -0,0 +1,17 @@
+data from db about the books and its gener etc
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+  ];
+
+// filter according to the problem statement 
+
+const node1 = books.filter( (prop)=> books.gener === "Non-Fiction");
+conole.log(node1);
