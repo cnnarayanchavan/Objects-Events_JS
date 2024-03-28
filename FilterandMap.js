8f562700716c965709f161adcc3d64ee4b7ab7e7
NARAYAN CHAVAN <113028954+cnnarayanchavan@users.noreply.github.com>
2024-03-28 13:18:35 +0530

    Update FilterandMap.js_ PS:03

--- FilterandMap.js
+++ FilterandMap.js
@@ -1,14 +1,14 @@
 //data from db about the books and its gener etc
 const books = [ 
-    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
-    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
-    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
-    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
-    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
-    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
-    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
-    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
-    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+    { title: 'Book One', genre: 'Fiction', publish: 1981, price: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, price: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 2000, price: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, price: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, price: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1981, price: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1981, price: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, price: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 2001, price: 1989 },
   ];
 
 // filter according to the problem statement 
@@ -24,3 +24,13 @@ const node2= books.filter( (bk2)=>{
   return  bk2.genre === "Non-Fiction" || bk2.edition ==2010
 } )
 console.log(node2);
+
+
+
+//Now make the process more optimized by using " reduce " higher order function
+//it includes "acc" "currVal" "initialVal"
+
+// problem statement : print the total of price of books whose year of publich in between 1900 to 2000
+
+const node3 = books.map( (temp1)=>{ return temp1.publish>=1900 || temp1.publish<2000} )
+console.log(node3)
